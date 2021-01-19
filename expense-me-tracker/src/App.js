d6d95eb5541fd009cd102b0dbc5bdca5060496e7
Michelle Wong <michellekwong2@gmail.com>
2021-01-19 13:54:45 -0500

    Dark Mode State

--- expense-me-tracker/src/App.js
+++ expense-me-tracker/src/App.js
@@ -4,22 +4,45 @@ import data from './data.js';
 import ExpenseListItem from './ExpenseListItem';
 
 class App extends React.Component{
-  renderExpenses(){
-      return data.map(expense => {
-        return <ExpenseListItem key={expense.id} expense={expense}/>
+  state = {
+    backgroundColor: 'white',
+    fontColor: 'black',
+    darkModeBtn: 'Dark Mode'
+  }
+
+  darkMode=()=>{
+    if(this.state.backgroundColor === 'white'){
+      this.setState({
+        backgroundColor: 'black',
+        fontColor: 'white',
+        darkModeBtn: 'Turn off Dark Mode'
+      })
+    } else {
+      this.setState({
+        backgroundColor:'white',
+        fontColor: 'black',
+        darkModeBtn: 'Dark Mode'
       })
     }
+  }
 
-    budgetReview(){
-      //render total savings, total expenses & monthly income
-      //Subtract expenses from income?
-    }
+  renderExpenses=()=>{
+    return data.map(expense => {
+      return <ExpenseListItem key={expense.id} expense={expense}/>
+    })
+  }
+
+  budgetReview=()=>{
+    //render total savings, total expenses & monthly income
+    //Subtract expenses from income?
+  }
 
   //show the month & previous months in nav bar
 
   render(){
     return (
-    <div className="App">
+    <div className="App" style={{backgroundColor: this.state.backgroundColor, color:this.state.fontColor}}>
+      <button onClick={this.darkMode}>{this.state.darkModeBtn}</button>
       <h1>Welcome to your Expense-Me-Tracker</h1>
       {this.renderExpenses()}
     </div>
