81c1739211d44b32cdc66cfd180c1acb4e4e191d
Michelle Wong <michellekwong2@gmail.com>
2021-01-18 19:26:07 -0500

    Expense data in data.js & render data

---
+++ expense-me-tracker/src/data.js
@@ -0,0 +1,22 @@
+const data = [
+  {
+    id: 1,
+    name: "Michelle",
+    monthly_savings: {
+      emergency_fund: 0,
+      investments: 0,
+      retirement: 0
+    },
+    monthly_expenses: {
+      food: 0,
+      clothing: 0,
+      housing: 0,
+      medical: 0,
+      transportation: 0,
+      personal: 0
+    },
+    monthly_income: 100
+  }
+]
+
+export default data;
